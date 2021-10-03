fd91c39c0e2e2c953b40bdd9af2adbd740d2fab2
emilibadov <emilen88@gmail.com>
2021-10-03 15:39:29 -0400

    made car james master branch

--- src/setup/Car.java
+++ src/setup/Car.java
@@ -5,5 +5,5 @@ public class Car {
 
     // we will have conflict
 
-    // because there is different text in different branches for car
+    // because there is different text in different branches for car and poh
 }
